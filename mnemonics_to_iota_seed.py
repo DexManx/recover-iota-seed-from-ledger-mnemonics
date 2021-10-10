24859d92b12137970c089164a742263331f5a6a5
David Manning <DETManning@Outlook.com>
2021-10-10 14:33:01 +1000

    adding unversioned files

--- mnemonics_to_iota_seed.py
+++ mnemonics_to_iota_seed.py
@@ -1,22 +1,26 @@
 # -*- coding: utf-8 -*-
-from mnemonics_utils import MnemonicsToIotaSeed, InputRecoveryWords, InputPassphrase
+from mnemonics_utils import MnemonicsToIotaSeed, InputRecoveryWords, InputPassphrase, InputLedgerStartIndex
 
-#===============================================================================
+
+# ===============================================================================
 def RecoverSeed():
     print("\nWelcome to IOTA Ledger Nano seed recovery!")
 
     recovery_words = InputRecoveryWords()
-    if recovery_words == None:
+    if recovery_words is None:
         return
 
     passphrase = InputPassphrase()
-    if passphrase == None:
+    if passphrase is None:
         return
 
+    trinity_index = int(InputLedgerStartIndex() or 0)
+
     print("\nCalculating your IOTA seed...")
-    iota_seed = MnemonicsToIotaSeed(recovery_words, passphrase)
+    iota_seed = MnemonicsToIotaSeed(recovery_words, passphrase, trinity_index)
     print("Seed: %s, Length: %d" % (iota_seed, len(iota_seed)))
 
-#===============================================================================
+
+# ===============================================================================
 if __name__ == '__main__':
     RecoverSeed()
